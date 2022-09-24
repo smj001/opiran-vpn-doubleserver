c5f4f8fd2cef1175223082d6bf457a9025f39ea9
smj <s.m.javadi001@gmail.com>
2022-09-24 16:57:37 +0330

    change - change ir port to udp

--- ir/install.sh
+++ ir/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 docker volume create --name opiran-service
-docker run -v opiran-service:/etc/openvpn --log-driver=none --rm kylemanna/openvpn ovpn_genconfig -u tcp://$1
+docker run -v opiran-service:/etc/openvpn --log-driver=none --rm kylemanna/openvpn ovpn_genconfig -u udp://$1
 docker run -v opiran-service:/etc/openvpn --log-driver=none --rm -it kylemanna/openvpn ovpn_initpki
-docker run -v opiran-service:/etc/openvpn -d -p 443:1194 --name opiran-ovpn --cap-add=NET_ADMIN kylemanna/openvpn
+docker run -v opiran-service:/etc/openvpn -d -p 443:1194/udp -p 443:1194/tcp --name opiran-ovpn --cap-add=NET_ADMIN kylemanna/openvpn
 docker run -v opiran-service:/etc/openvpn --log-driver=none --rm -it kylemanna/openvpn easyrsa build-client-full opiran nopass
 docker run -v opiran-service:/etc/openvpn --log-driver=none --rm kylemanna/openvpn ovpn_getclient opiran > ~/opiran-$1.ovpn
 sed -i 's/1194/443/g' ~/opiran-$1.ovpn
@@ -16,9 +16,9 @@ if [[ -n $4 ]]; then
     ip route add $4 via $2
 fi
 
-for rage in $(cat /tmp/ir-ip-range.txt)
+for range in $(cat /tmp/ir-ip-range.txt)
 do
-    ip route add $i via $2
+    ip route add $range via $2
 done
 
 sysctl -w net.ipv4.ip_forward=1
