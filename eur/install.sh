f486c8c00a08a60f6e90c6c119f2382e51317d0f
smj <s.m.javadi001@gmail.com>
2022-09-24 08:30:47 +0330

    change - change port after create ovpn file

--- eur/install.sh
+++ eur/install.sh
@@ -4,4 +4,5 @@ docker run -v opiran-service:/etc/openvpn --log-driver=none --rm kylemanna/openv
 docker run -v opiran-service:/etc/openvpn --log-driver=none --rm -it kylemanna/openvpn ovpn_initpki
 docker run -v opiran-service:/etc/openvpn -d -p 443:1194 --name opiran-ovpn --cap-add=NET_ADMIN kylemanna/openvpn
 docker run -v opiran-service:/etc/openvpn --log-driver=none --rm -it kylemanna/openvpn easyrsa build-client-full opiran nopass
-docker run -v opiran-service:/etc/openvpn --log-driver=none --rm kylemanna/openvpn ovpn_getclient opiran > opiran-$1.ovpn
+docker run -v opiran-service:/etc/openvpn --log-driver=none --rm kylemanna/openvpn ovpn_getclient opiran > ~/opiran-$1.ovpn
+sed -i 's/1194/443/g' ~/opiran-$1.ovpn
---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+EU_ADDR
+
+EU_FILE
+IR_ADDR
+IR_GW
+YOUR_IP
+
