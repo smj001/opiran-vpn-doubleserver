93048b8be82f94c7191bf5f7b9ef418b5f7d5c53
smj <s.m.javadi001@gmail.com>
2022-09-24 07:12:35 +0330

    bugfix - revert changes and change port

--- eur/install.sh
+++ eur/install.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 docker volume create --name opiran-service
 docker run -v opiran-service:/etc/openvpn --log-driver=none --rm kylemanna/openvpn ovpn_genconfig -u tcp://$1
-docker run -v opiran-service:/etc/openvpn --log-driver=none --rm -it kylemanna/openvpn ovpn_initpki << EOF
-opiran
-opiran
-opiran
-opiran
-opiran
-EOF
-docker run -v opiran-service:/etc/openvpn -d -p 53:1194 --name opiran-ovpn --cap-add=NET_ADMIN kylemanna/openvpn
-docker run -v opiran-service:/etc/openvpn --log-driver=none --rm -it kylemanna/openvpn easyrsa build-client-full $2 nopass << EOF
-opiran
-EOF
-docker run -v opiran-service:/etc/openvpn --log-driver=none --rm kylemanna/openvpn ovpn_getclient $2 > $2.ovpn
+docker run -v opiran-service:/etc/openvpn --log-driver=none --rm -it kylemanna/openvpn ovpn_initpki
+docker run -v opiran-service:/etc/openvpn -d -p 443:1194 --name opiran-ovpn --cap-add=NET_ADMIN kylemanna/openvpn
+docker run -v opiran-service:/etc/openvpn --log-driver=none --rm -it kylemanna/openvpn easyrsa build-client-full opiran nopass
+docker run -v opiran-service:/etc/openvpn --log-driver=none --rm kylemanna/openvpn ovpn_getclient opiran > opiran-$1.ovpn
